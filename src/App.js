54dabbcbf3b0a366732a3c99165ee8966ced7444
Vitor Ruan de Souza <vitorruann@gmail.com>
2021-03-11 12:16:53 -0300

    modeule

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import './App.css';
+import { Form, Input } from '@rocketseat/unform';
+
+const socket = io('http://localhost:3005');
 
 function App() {
+  const [idSocket, setIdSocket] = useState();
+  const [allMessages, setAllMessages] = useState([]);
+  let receivedMessages = [];
+
+
+  useEffect(() => {
+    function defaultFunc() {
+      loadAllMessages();
+      console.log('useEffetc receivedMessages- ', receivedMessages, " - allMessages", allMessages);
+    }
+    defaultFunc();
+  },[]);
+
+  function loadAllMessages() {
+    socket.on('id', (id) => setIdSocket(id));
+    console.log('loadAllMessages - id');
+
+    // socket.on('receivedMessage', (data) => setAllMessages(data))
+    socket.on('receivedMessage', function(data) {
+      
+      receivedMessages.push(data);
+      setAllMessages(receivedMessages);
+      console.log('receivedMessage - ', receivedMessages," - allMessages", allMessages );
+
+    }); 
+  }
+
+  function sendMessage(data) {
+    socket.emit('sendMessage', data);
+    receivedMessages.push(data);
+    setAllMessages(receivedMessages);
+    console.log('sendMessages - ', receivedMessages," - allMessages", allMessages);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Form onSubmit={sendMessage} className="formChat">
+        <label htmlFor="">
+          ID da conexão: { idSocket }
+        </label>
+        <div className="content">
+          <div>
+            <Input type="text" name="name" placeholder="Dígite seu nome"/>
+
+            <div className="allMessage">
+              {allMessages.map(data => (
+                <div>
+                  <strong>{data.name}: </strong>
+                  <label htmlFor="">{data.msg}</label>
+                  {console.log(data.name)}
+                </div>
+              ))}
+            </div>
+
+            <Input type="text" name="msg" placeholder="Dígite a mensagem"/>
+
+            <button type="submit">Enviar</button>
+          
+          </div>
+        </div>
+      </Form>
     </div>
   );
 }
